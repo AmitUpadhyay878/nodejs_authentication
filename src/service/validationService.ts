3ecd37ba52a816b08e5320b743c806248a672deb
amitmq <amit.multiqos@gmail.com>
2024-12-02 11:23:16 +0530

    new chnages done

--- src/service/validationService.ts
+++ src/service/validationService.ts
@@ -1,7 +1,7 @@
 import joi from 'joi'
 import { IRegisterRequestBody } from '../types/userTypes'
 
-export const validationRegisterBody =joi.object<IRegisterRequestBody>({
+export const validationRegisterBody =joi.object<IRegisterRequestBody,true>({
         name:joi.string().min(2).max(72).trim().required(),
         email:joi.string().email().required(),
         phoneNumber:joi.string().min(4).max(20).required(),
@@ -13,7 +13,8 @@ export const validationSchema = <T>(schema:joi.Schema,value:unknown)=>{
         const result = schema.validate(value)
 
         return{
-            value: result.value as  
+            value: result.value as T,
+            error: result.error
         }
         
 }
